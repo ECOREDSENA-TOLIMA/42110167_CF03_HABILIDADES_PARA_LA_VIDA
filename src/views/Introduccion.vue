c21daa7781f924c4f12adde953e7fe9faab00537
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2024-12-13 18:45:26 -0500

    Video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -28,7 +28,7 @@
     
     figure.mb-5(data-aos="zoom-in")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/vBSo9QLX5a4?si=Ij4wG6OvZJqRaxkT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     
 </template>
